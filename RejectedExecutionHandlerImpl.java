6c4f5700deb63610f8b4249536e5f4a7749513e2
Dmitry Ivanov <dmitriy_ivanov98@mail.ru>
2025-05-25 20:31:29 +0300

    Update RejectedExecutionHandlerImpl.java

--- RejectedExecutionHandlerImpl.java
+++ RejectedExecutionHandlerImpl.java
@@ -10,12 +10,7 @@ public class RejectedExecutionHandlerImpl implements RejectedExecutionHandler {
 
     @Override
     public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
-         LOGGER.severe("[Rejected] Task " + r.toString() + " was rejected due to overload!");
-        // Можно реализовать разные стратегии:
-        // 1. Отклонение задачи (как сейчас)
-        // 2. Выполнение задачи в текущем потоке: r.run();
-        // 3. Сброс самой старой задачи из очереди и добавление новой
-        // 4. Реализация backpressure (например, с использованием Reactive Streams)
+         LOGGER.severe("[Rejected] Task " + r.toString() + " was rejected due to overload!")
 
     }
 
